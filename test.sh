80d34c3896ffd7274b3f1cf5ecfd57aa25fdc79b
tmlbl <tim@shippable.com>
2014-08-12 00:34:56 -0700

    more

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-docker run -v $(pwd):/app -w /app --entrypoint /bin/sh koa /app/boot_test.sh
+docker run -v $(pwd):/app --name koa_test -w /app --entrypoint /bin/sh koa /app/boot_test.sh
+docker rm koa_test
 
